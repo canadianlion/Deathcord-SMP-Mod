1be07675e926015015d20d38a129278921e80295
canadianlion <william.loewen111@gmail.com>
2026-02-26 15:59:04 -0700

    Start of Mod

---
+++ src/main/java/net/canadianlion/deathcordsmpmod/DeathcordSMPMod.java
@@ -0,0 +1,16 @@
+package net.canadianlion.deathcordsmpmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeathcordSMPMod implements ModInitializer {
+	public static final String MOD_ID = "deathcord-smp-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/canadianlion/deathcordsmpmod/DeathcordSMPModClient.java
@@ -0,0 +1,10 @@
+package net.canadianlion.deathcordsmpmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeathcordSMPModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
