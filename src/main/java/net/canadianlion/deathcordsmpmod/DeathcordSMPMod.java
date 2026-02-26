05ca2b288a01bdf9082c644ade146d95b12c7cde
canadianlion <william.loewen111@gmail.com>
2026-02-26 16:13:31 -0700

    Initial Commit

--- src/main/java/net/canadianlion/deathcordsmpmod/DeathcordSMPMod.java
+++ src/main/java/net/canadianlion/deathcordsmpmod/DeathcordSMPMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DeathcordSMPMod implements ModInitializer {
-	public static final String MOD_ID = "deathcord-smp-mod";
+	public static final String MOD_ID = "deathcordsmpmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
